33cd9336a0f3a7067b634aa288eb277d60faca8f
ELMAZOUGH <yassine.elmazoughi-etu@etu.univh2c.ma>
2026-01-06 02:13:51 +0100

    Ajout du serveur Flask

--- app2.py
+++ app2.py
@@ -8,5 +8,5 @@ def home():
     return "Hello, DevOps!"
 
 if __name__ == "__main__":
-    # Il faut 4 espaces ici pour être DANS la condition if
+    # Il faut http://localhost:5000/4 espaces ici pour être DANS la condition if
     app.run(host="0.0.0.0", port=5000)
